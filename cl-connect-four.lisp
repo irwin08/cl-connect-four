892d11c71ec04e60c5a58dc1269a078aea124213
Jesse Irwin <jesse.irwin11@gmail.com>
2022-07-24 15:13:02 -0700

    Fix victory checking

--- cl-connect-four.lisp
+++ cl-connect-four.lisp
@@ -74,21 +74,22 @@
   (let ((upward '())
 	(downward '())
 	(offset 0))
-
     ;; upward sloping
-    (loop while (and (nth (+ row offset) board) (nth (+ col offset) board))
+    (loop while (nth (+ row offset) (nth (+ col offset) board))
 	  do (setq upward (append upward (list offset)))
 	     (incf offset))
-    (setf offset 0)
+    ;; set to 1 so as to not double count starting position
+    (setf offset 1)
     (loop while (and (>= (- row offset) 0) (>= (- col offset) 0) (nth (- row offset) board) (nth (- col offset) board))
 	  do (setq upward (append upward (list (- 0 offset))))
 	     (incf offset))
     (setf offset 0)
     ;; downward sloping
-    (loop while (and (>= (- row offset) 0) (nth (- row offset) board) (nth (+ col offset) board))
+    (loop while (nth (+ row offset) (nth (+ col offset) board))
 	  do (setq downward (append downward (list offset)))
 	     (incf offset))
-    (setf offset 0)
+    ;; again, set to 1 so as to not double count starting position
+    (setf offset 1)
     (loop while (and (>= (- col offset) 0)(nth (+ row offset) board) (nth (- col offset) board))
 	  do (setq downward (append downward (list (- 0 offset))))
 	     (incf offset))
@@ -99,12 +100,12 @@
 	(current-token nil))
     (loop for offset in (nth 0 (get-diagonal-range board row col))
 	  do (if (equal current-token (nth (+ row offset) (nth (+ col offset) board)))
-		 (setq count (+ 1 count))
+		 (progn (setq count (+ 1 count))
+			(if (>= count 4)
+			    (return-from check-victory-diagonal current-token)
+			    nil))
 		 (progn (setq count 1)
-			(setq current-token (nth (+ row offset) (nth (+ col offset) board)))))
-	     (if (>= count 4)
-		 (return current-token)
-		 nil))
+			(setq current-token (nth (+ row offset) (nth (+ col offset) board))))))
     (setq count 0)
     (loop for offset in (nth 1 (get-diagonal-range board row col))
 	  do (if (equal current-token (nth (+ row offset) (nth (+ col offset) board)))
@@ -112,12 +113,26 @@
 		 (progn (setq count 1)
 			(setq current-token (nth (+ row offset) (nth (+ col offset) board)))))
 	     (if (>= count 4)
-		 (return current-token)
+		 (return-from check-victory-diagonal current-token)
 		 nil))))
+
+
+;; TODO!!! Make macro for if expression that stores expressions in or form and returns value of first not nil case
 
 ;; checks if board state is victory - takes board, and row and col of latest move
 ;; returns winning token if true, nil otherwise
 (defun check-victory (board row col)
   (if (or (check-victory-vertical board row col) (check-victory-horizontal board row col) (check-victory-diagonal board row col))
-      t
+      (or (check-victory-vertical board row col) (check-victory-horizontal board row col) (check-victory-diagonal board row col))
+      nil))
+
+(defun get-opposite-token (token)
+  (if (equal token 'R1)
+      'R2
+      'R1))
+
+;; checks if making a move at given row/column is legal
+(defun check-legal (board row col token)
+  (if (and (not (nth row (nth col board)))(or (= 0 row) (nth (- row 1) (nth col board))))
+      (not (equal (check-victory board row col) (get-opposite-token token)))
       nil))
