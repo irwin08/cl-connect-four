d0c10764fb5a0dceb670c27f5c2d955988930372
Jesse <jesse.irwin11@gmail.com>
2022-07-23 18:46:05 -0700

    Update

--- cl-connect-four.lisp
+++ cl-connect-four.lisp
@@ -68,7 +68,56 @@
 		 (return current-token)
 		 nil))))
 
+
+;; need to get both upward sloping and downward sloping offset
+(defun get-diagonal-range (board row col)
+  (let ((upward '())
+	(downward '())
+	(offset 0))
+
+    ;; upward sloping
+    (loop while (and (nth (+ row offset) board) (nth (+ col offset) board))
+	  do (setq upward (append upward (list offset)))
+	     (incf offset))
+    (setf offset 0)
+    (loop while (and (>= (- row offset) 0) (>= (- col offset) 0) (nth (- row offset) board) (nth (- col offset) board))
+	  do (setq upward (append upward (list (- 0 offset))))
+	     (incf offset))
+    (setf offset 0)
+    ;; downward sloping
+    (loop while (and (>= (- row offset) 0) (nth (- row offset) board) (nth (+ col offset) board))
+	  do (setq downward (append downward (list offset)))
+	     (incf offset))
+    (setf offset 0)
+    (loop while (and (>= (- col offset) 0)(nth (+ row offset) board) (nth (- col offset) board))
+	  do (setq downward (append downward (list (- 0 offset))))
+	     (incf offset))
+    (list upward downward)))
+
+(defun check-victory-diagonal (board row col)
+    (let ((count 0)
+	(current-token nil))
+    (loop for offset in (nth 0 (get-diagonal-range board row col))
+	  do (if (equal current-token (nth (+ row offset) (nth (+ col offset) board)))
+		 (setq count (+ 1 count))
+		 (progn (setq count 1)
+			(setq current-token (nth (+ row offset) (nth (+ col offset) board)))))
+	     (if (>= count 4)
+		 (return current-token)
+		 nil))
+    (setq count 0)
+    (loop for offset in (nth 1 (get-diagonal-range board row col))
+	  do (if (equal current-token (nth (+ row offset) (nth (+ col offset) board)))
+		 (setq count (+ 1 count))
+		 (progn (setq count 1)
+			(setq current-token (nth (+ row offset) (nth (+ col offset) board)))))
+	     (if (>= count 4)
+		 (return current-token)
+		 nil))))
+
 ;; checks if board state is victory - takes board, and row and col of latest move
 ;; returns winning token if true, nil otherwise
 (defun check-victory (board row col)
-  )
+  (if (or (check-victory-vertical board row col) (check-victory-horizontal board row col) (check-victory-diagonal board row col))
+      t
+      nil))
